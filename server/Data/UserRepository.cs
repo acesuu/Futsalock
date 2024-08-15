8acfd616b798b0986e758e7f6573ef99ccc65cdc
acesuu <ahnafakib@iut-dhaka.edu>
2024-08-16 01:37:03 +0600

    created a jwt util class for jwt creation and created UserRepostory to add a new user and check user credentials

---
+++ server/Data/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using server.Data;
+using server.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+public class UserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User> AddUserAsync(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<User> GetUserAsync(string username, string password)
+    {
+        // checking the password hash.
+        return await _context.Users.FirstOrDefaultAsync(u => u.Username == username && u.PasswordHash == password);
+    }
+}
