f816b8cbda56f692e9ab8767273cc89585f7eaf9
acesuu <ahnafakib@iut-dhaka.edu>
2024-08-16 04:04:47 +0600

    added GroundRepository for getting the available ground list

---
+++ server/Data/GroundRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data
+{
+    public class GroundRepository
+    {
+        private readonly AppDbContext _context;
+
+        public GroundRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Ground>> GetAvailableGroundsAsync(DateTime date)
+        {
+            return await _context.Grounds
+                .Include(g => g.Bookings)
+                .Where(g => g.IsActive)
+                .ToListAsync();
+        }
+    }
+}
