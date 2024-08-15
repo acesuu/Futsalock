6b7945b386a1d0a9f7cb7159442587f55e166de1
acesuu <ahnafakib@iut-dhaka.edu>
2024-08-16 05:47:04 +0600

    added dto for booking feature

---
+++ server/Dto/CreateBookingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Dto
+{
+    public class CreateBookingDto
+    {
+        public int GroundId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ server/Dto/UpdateBookingStatusDto.cs
@@ -0,0 +1,8 @@
+﻿namespace server.Dto
+{
+    public class UpdateBookingStatusDto
+    {
+        public int BookingId { get; set; }
+        public string Status { get; set; } 
+    }
+}
