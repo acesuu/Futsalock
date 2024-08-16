c335aceeb86e8ec3a7e3848d3d12c0107ee7e3c2
acesuu <ahnafakib@iut-dhaka.edu>
2024-08-16 07:27:59 +0600

    completed auth module and routing

--- client/src/app/auth/auth-routing.module.ts
+++ client/src/app/auth/auth-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignoutComponent } from './signout/signout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'signout', component: SignoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignoutComponent } from './signout/signout.component';
 import { AuthRoutingModule } from './auth-routing.module';
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+    
+  ],
   imports: [
+    SigninComponent,
+    SignupComponent,
+    SignoutComponent,
     CommonModule,
     AuthRoutingModule
   ]
